b99163a5df721ac5c99b86fd44a69bbd53f66ec8
Eschamali <mokkun75@gmail.com>
2026-06-29 19:32:48 +0900

    ブランチ名の修正

--- SubtreeFetch.bat
+++ SubtreeFetch.bat
@@ -7,7 +7,7 @@ git fetch https://github.com/Eschamali/StarterWebScrapingKit.git main
 git subtree pull --prefix=Original/Chromium-Automation-with-CDP-for-VBA https://github.com/longvh211/Chromium-Automation-with-CDP-for-VBA.git main
 git subtree pull --prefix=Original/ChromeControler-No-Selenium-WebDriver-VBAJSON https://github.com/24000/ChromeControler-No-Selenium-WebDriver-VBAJSON.git master
 git subtree pull --prefix=Original/VBA-WEB https://github.com/VBA-Tools-v2/VBA-Web.git master
-git subtree pull --prefix=Original/VBA-FastJSON https://github.com/cristianbuse/VBA-FastJSON.git main
+git subtree pull --prefix=Original/VBA-FastJSON https://github.com/cristianbuse/VBA-FastJSON.git master
 git subtree pull --prefix=Original/vbacollective-json https://github.com/vbacollective/json.git main
 
 REM assetì‡ÇÃçXêV
