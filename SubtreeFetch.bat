d308333c4e9fe0d31411cd7fbd4ed9aa89642e17
Eschamali <mokkun75@gmail.com>
2026-06-29 19:30:51 +0900

    参照元の変更

--- SubtreeFetch.bat
+++ SubtreeFetch.bat
@@ -7,7 +7,8 @@ git fetch https://github.com/Eschamali/StarterWebScrapingKit.git main
 git subtree pull --prefix=Original/Chromium-Automation-with-CDP-for-VBA https://github.com/longvh211/Chromium-Automation-with-CDP-for-VBA.git main
 git subtree pull --prefix=Original/ChromeControler-No-Selenium-WebDriver-VBAJSON https://github.com/24000/ChromeControler-No-Selenium-WebDriver-VBAJSON.git master
 git subtree pull --prefix=Original/VBA-WEB https://github.com/VBA-Tools-v2/VBA-Web.git master
-git subtree pull --prefix=Original/SeleniumVBA https://github.com/GCuser99/SeleniumVBA.git main
+git subtree pull --prefix=Original/VBA-FastJSON https://github.com/cristianbuse/VBA-FastJSON.git main
+git subtree pull --prefix=Original/vbacollective-json https://github.com/vbacollective/json.git main
 
 REM asset“à‚ÌXV
 curl https://cdn.jsdelivr.net/npm/chromium-bidi@latest/lib/iife/mapperTab.js > assset\mapperTab.js
